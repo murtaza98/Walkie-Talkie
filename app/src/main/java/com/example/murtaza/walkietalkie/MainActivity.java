62c247c29b3b649928da4ec205c26412bc5ce712
murtaza98 <m.patrawala@somaiya.edu>
2019-03-09 12:44:53 +0530

    Added basic P2P functions

--- app/src/main/java/com/example/murtaza/walkietalkie/MainActivity.java
+++ app/src/main/java/com/example/murtaza/walkietalkie/MainActivity.java
@@ -1,13 +1,332 @@
 package com.example.murtaza.walkietalkie;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnOnOff, btnDiscover, btnSend;
+    ListView listView;
+    TextView readMsgBox, connectionStatus;
+    EditText writeMsg;
+
+    WifiManager wifiManager;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    public static final int PORT_USED = 8889;
+
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+    List<WifiP2pDevice> peers = new ArrayList<>();
+    String[] deviceNameArray;
+    WifiP2pDevice[] deviceArray;
+
+    static final int MESSAGE_READ = 1;
+
+    ServerClass serverClass;
+    ClientClass clientClass;
+    SendReceive sendReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate  (savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialSetup();
+        executeListeners();
+
+    }
+
+    public class ServerClass extends Thread{
+        Socket socket;
+        ServerSocket serverSocket;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(PORT_USED);
+                socket = serverSocket.accept();
+
+                SocketHandler.setSocket(socket);
+
+                startActivity(new Intent(getApplicationContext(), ChatWindow.class));
+
+
+
+                // start the sendReceive class
+//                Toast.makeText(getApplicationContext(), "Send Revieve started", Toast.LENGTH_SHORT).show();
+//                sendReceive = new SendReceive(socket);
+//                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class ClientClass extends Thread{
+        Socket socket;
+        String hostAddress;
+
+        ClientClass(InetAddress address){
+            this.socket = new Socket();
+            this.hostAddress = address.getHostAddress();
+        }
+
+        @Override
+        public void run() {
+            try {
+                socket.connect(new InetSocketAddress(hostAddress, PORT_USED), 500);
+
+                SocketHandler.setSocket(socket);
+
+                startActivity(new Intent(getApplicationContext(), ChatWindow.class));
+
+                // start the sendReceive class
+//                Toast.makeText(getApplicationContext(), "Send Revieve started", Toast.LENGTH_SHORT).show();
+//                sendReceive = new SendReceive(socket);
+//                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SendReceive extends Thread{
+        private Socket socket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+        public SendReceive(Socket socket){
+            this.socket = socket;
+            try {
+                this.inputStream = socket.getInputStream();
+                this.outputStream = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes_len;
+
+            while (socket != null){
+                try {
+                    bytes_len = inputStream.read(buffer);
+                    if(bytes_len > 0){
+                        handler.obtainMessage(MESSAGE_READ, bytes_len, -1, buffer).sendToTarget();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void write(byte[] messageBytes){
+            try {
+                outputStream.write(messageBytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case MESSAGE_READ:
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMessage = new String(readBuff, 0, msg.arg1);
+                    readMsgBox.append("\n"+tempMessage);
+                    break;
+            }
+            return true;
+        }
+    });
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    private void initialSetup() {
+        // layout files
+        btnOnOff = (Button) findViewById(R.id.onOff);
+        btnDiscover = (Button) findViewById(R.id.discover);
+        btnSend = (Button) findViewById(R.id.sendButton);
+        listView = (ListView) findViewById(R.id.peerListView);
+        readMsgBox = (TextView) findViewById(R.id.readMsg);
+        connectionStatus = (TextView) findViewById(R.id.connectionStatus);
+        writeMsg = (EditText) findViewById(R.id.writeMsg);
+
+        // add onClick Listeners
+        btnOnOff.setOnClickListener(this);
+        btnDiscover.setOnClickListener(this);
+        btnSend.setOnClickListener(this);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        mManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
+    private void executeListeners() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final WifiP2pDevice device = deviceArray[position];
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+
+                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "Connected to "+device.deviceName, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(getApplicationContext(), "Error in connecting to "+device.deviceName, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.onOff:
+                checkWifiState();
+                break;
+            case R.id.discover:
+                discoverDevices();
+                break;
+            case R.id.sendButton:
+                if(sendReceive == null)
+                    return;
+                // send the message
+                String msg = writeMsg.getText().toString();
+                sendReceive.write(msg.getBytes());
+                break;
+        }
+    }
+
+    private void discoverDevices() {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                connectionStatus.setText("Discovery Started");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                connectionStatus.setText("Discovery start Failed");
+            }
+        });
+    }
+
+    WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peersList) {
+            if(!peersList.getDeviceList().equals(peers)){
+                peers.clear();
+                peers.addAll(peersList.getDeviceList());
+
+                deviceNameArray = new String[peersList.getDeviceList().size()];
+                deviceArray = new WifiP2pDevice[peersList.getDeviceList().size()];
+
+                int index=0;
+                for(WifiP2pDevice device : peersList.getDeviceList()){
+                    deviceNameArray[index] = device.deviceName;
+                    deviceArray[index] = device;
+                    index++;
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
+                listView.setAdapter(adapter);
+            }
+
+            if(peers.size() == 0){
+                Toast.makeText(getApplicationContext(), "No Peers Found", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo info) {
+            final InetAddress groupOwnerAddress = info.groupOwnerAddress;
+
+            if(info.groupFormed && info.isGroupOwner){
+                connectionStatus.setText("HOST");
+//                Toast.makeText(getApplicationContext(), "Server class started", Toast.LENGTH_SHORT).show();
+                serverClass = new ServerClass();
+                serverClass.start();
+            }else if(info.groupFormed){
+                connectionStatus.setText("CLIENT");
+//                Toast.makeText(getApplicationContext(), "Client class started", Toast.LENGTH_SHORT).show();
+                clientClass = new ClientClass(groupOwnerAddress);
+                clientClass.start();
+            }
+        }
+    };
+
+    private void checkWifiState() {
+        if(wifiManager.isWifiEnabled()){
+            wifiManager.setWifiEnabled(false);
+            btnOnOff.setText("ON");
+        }else{
+            wifiManager.setWifiEnabled(true);
+            btnOnOff.setText("OFF");
+        }
     }
 }
---
+++ app/src/main/java/com/example/murtaza/walkietalkie/SocketHandler.java
@@ -0,0 +1,15 @@
+package com.example.murtaza.walkietalkie;
+
+import java.net.Socket;
+
+public class SocketHandler {
+    private static Socket socket;
+
+    public static synchronized Socket getSocket(){
+        return socket;
+    }
+
+    public static synchronized void setSocket(Socket socket){
+        SocketHandler.socket = socket;
+    }
+}
