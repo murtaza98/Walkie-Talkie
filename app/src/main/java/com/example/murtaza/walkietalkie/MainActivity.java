9ae165fefe9e8942f373a6766fade52dde149bf6
murtaza98 <m.patrawala@somaiya.edu>
2019-03-16 11:18:47 +0530

    Android Oreo+ support added to find peer list

--- app/src/main/java/com/example/murtaza/walkietalkie/MainActivity.java
+++ app/src/main/java/com/example/murtaza/walkietalkie/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.murtaza.walkietalkie;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
@@ -12,8 +15,9 @@ import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import android.provider.Settings;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -262,9 +266,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         Toast.makeText(getApplicationContext(), "Error in connecting to "+device.deviceName, Toast.LENGTH_SHORT).show();
                     }
                 });
-
             }
         });
+    }
+
+    void checkLocationEnabled(){
+        LocationManager lm = (LocationManager)MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(!gps_enabled && !network_enabled) {
+            // notify user
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.gps_network_not_enabled_title)
+                    .setMessage(R.string.gps_network_not_enabled)
+                    .setPositiveButton(R.string.open_location_settings, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                            MainActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton(R.string.Cancel,null)
+                    .show();
+        }
     }
 
     @Override
@@ -274,6 +306,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 checkWifiState();
                 break;
             case R.id.discover:
+                checkLocationEnabled();
                 discoverDevices();
                 break;
             case R.id.sendButton:
