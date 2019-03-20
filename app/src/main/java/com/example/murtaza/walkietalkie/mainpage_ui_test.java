ea8711ce5686a140ec3e5145002b68c8fb658477
murtaza98 <m.patrawala@somaiya.edu>
2019-03-21 01:05:23 +0530

    huge icon problem fixed

--- app/src/main/java/com/example/murtaza/walkietalkie/mainpage_ui_test.java
+++ app/src/main/java/com/example/murtaza/walkietalkie/mainpage_ui_test.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.skyfishjy.library.RippleBackground;
 
@@ -30,21 +31,29 @@ public class mainpage_ui_test extends AppCompatActivity {
 
         final Handler handler=new Handler();
 
-        View device1 = LayoutInflater.from(this).inflate(R.layout.device_icon, null);
+        final View device1 = LayoutInflater.from(this).inflate(R.layout.device_icon, null);
 //        device1.setLayoutParams(new LinearLayout.LayoutParams(
 //                64,
 //                64));
         rippleBackground.addView(device1);
-        View device2 = LayoutInflater.from(this).inflate(R.layout.device_icon, null);
-//        device2.setLayoutParams(new LinearLayout.LayoutParams(
-//                64,
-//                64));
-        rippleBackground.addView(device2);
 
-        final View[] device_array = {device1, device2};
-        final Float[] float_value1 = {0f, 0f};
-        final Float[] float_value2 = {1.2f, 1.2f};
-        final Float[] float_value3 = {1f, 1f};
+//        final TextView device1 = new TextView(this);
+//        RippleBackground.LayoutParams params = new RippleBackground.LayoutParams(180,180);
+//        params.setMargins(20, 50, 0,0);
+//
+//        v1.setLayoutParams(params);
+//        v1.setBackgroundResource(R.drawable.phone2);
+//        v1.setVisibility(View.INVISIBLE);
+//        v1.setText("LENOVO K8 Note");
+//        rippleBackground.addView(v1);
+
+//        View device2 = LayoutInflater.from(this).inflate(R.layout.device_icon, null);
+////        device2.setLayoutParams(new LinearLayout.LayoutParams(
+////                64,
+////                64));
+//        rippleBackground.addView(device2);
+
+        final View[] device_array = {device1};
 
 //        foundDevice=(ImageView)findViewById(R.id.foundDevice);
         ImageView button=(ImageView)findViewById(R.id.centerImage);
@@ -55,23 +64,24 @@ public class mainpage_ui_test extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        int index = count % device_array.length;
-                        foundDevice(device_array[index], float_value1[index], float_value2[index], float_value3[index]);
-                        count++;
+//                        int index = count % device_array.length;
+//                        foundDevice(device_array[index]);
+//                        count++;
+                        foundDevice(device1);
                     }
-                },2000);
+                },1000);
             }
         });
     }
 
-    private void foundDevice(View foundDevice, float value1, float value2, float value3){
+    private void foundDevice(View foundDevice){
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.setDuration(400);
         animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
         ArrayList<Animator> animatorList=new ArrayList<Animator>();
-        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleX", value1, value2, value3);
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleX", 0f, 1.2f, 1f);
         animatorList.add(scaleXAnimator);
-        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleY", value1, value2, value3);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleY", 0f, 1.2f, 1f);
         animatorList.add(scaleYAnimator);
         animatorSet.playTogether(animatorList);
         foundDevice.setVisibility(View.VISIBLE);
